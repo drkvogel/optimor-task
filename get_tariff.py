d3b8606ce969ade389c184669e285bdcaa5ba476
Chris Bird <chrisjbird@gmail.com>
2016-03-09 23:39:07 +0000

    more

--- get_tariff.py
+++ get_tariff.py
@@ -9,9 +9,12 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 import time
+import logging
 
 def get_o2_landline_tariff(country, browser_name):
+
     url = 'http://international.o2.co.uk/internationaltariffs/calling_abroad_from_uk'
+
     if browser_name == 'Firefox':
         browser = webdriver.Firefox()
     else:
@@ -19,46 +22,38 @@ def get_o2_landline_tariff(country, browser_name):
 
     # load intial page
     browser.get(url)
-    #assert "O2" in browser.title
 
     # select country #countryName //*[@id="countryName"]
-    elem = browser.find_element_by_xpath('//*[@id="countryName"]')
+    logging.info("find #countryName")
+    elem = browser.find_element_by_xpath('//*[@id="countryNa me"]')
     elem.send_keys(country)
     elem.send_keys(Keys.RETURN)
-    # assert that there is now li.btn_arrow_tab a#paymonthly
-    # <li class="btn_arrow_tab left active  ui-state-default ui-corner-top">
-    #     <a href="#paymonthlyTariffPlan" id="paymonthly" class="doubleText">Pay Monthly<small>View standard rates and Bolt Ons</small></a>
-    # </li>
     time.sleep(0.2)
-    raise Exception("countryName")
 
     # select Pay Monthly #paymonthly //*[@id="paymonthly"]
-    #elem = browser.find_element_by_name("paymonthly") # no, that's the id, not the name - name is the element type, e.g. p or a or li
-    #browser.find_element_by_xpath("//a[contains(@href,'http://seleniumhq.org')]")
-
+    logging.info("find #paymonthly")
     elem = browser.find_element_by_xpath('//*[@id="paymonthly"]')
-    #elem.send_keys(country)
-    elem.send_keys(Keys.RETURN)
+    elem.click()
     time.sleep(0.2)
-    # raise Exception("paymonthly")
 
     # find tariff in #standardRatesTable //*[@id="standardRatesTable"]/tbody/tr[1]/td[2]
+    logging.info("find tariff in #standardRatesTable")
     landline = browser.find_element_by_xpath('//*[@id="standardRatesTable"]/tbody/tr[1]/td[2]')
-    raise Exception("standardRatesTable")
 
-    #assert "No results found." not in browser.page_source
-    # browser.close()
-    return landline
+    browser.close()
+    return landline.text
 
 
-countries = ["Canada", "Germany", "Iceland", "Pakistan", "Singapore", "South Africa"]
+#countries = ["Canada", "Germany", "Iceland", "Pakistan", "Singapore", "South Africa"]
+countries = ["Canada", "Germany"]
 
 for country in countries:
+    print "Getting landline tariff for " + country
     try:
-        print "O2's landline tariff for" + country + " is: " + get_o2_landline_tariff(country, 'Firefox')
+        print "O2's landline tariff for " + country + " is: " + get_o2_landline_tariff(country, 'Firefox')
     except NoSuchElementException as ex:
         # e.g. NoSuchElementException: Message: Unable to locate element: {"method":"name","selector":"countryName"}
-        print "NoSuchElementException: " + str(ex.args)
+        print "NoSuchElementException: " + ex.message #str(ex.args)
     except Exception as ex:
         print "An error occurred whilst trying to retrieve a tariff for " + country
         print "Message: " + str(ex.args)
