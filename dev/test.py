d02c69e0989dd8f228249e26e6d68af462a00e70
Chris Bird <chrisjbird@gmail.com>
2016-03-09 22:31:51 +0000

    more

--- dev/test.py
+++ dev/test.py
@@ -8,19 +8,13 @@ Created on Wed Mar  9 21:45:08 2016
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-countries = ["Canada", "Germany", "Iceland", "Pakistan", "Singapore", "South Africa"]
-url = 'http://international.o2.co.uk/internationaltariffs/calling_abroad_from_uk'
-
-# #countryName
-# #paymonthly
-# #standardRatesTable
-
-def get_landline_tariff(country):
+def get_o2_landline_tariff(country):
+    url = 'http://international.o2.co.uk/internationaltariffs/calling_abroad_from_uk'
     driver = webdriver.Firefox()
     driver.get(url)
     #assert "Python" in driver.title
 
-    # select country
+    # select country #countryName
     elem = driver.find_element_by_name("countryName")
     elem.send_keys(country)
     elem.send_keys(Keys.RETURN)
@@ -28,27 +22,32 @@ def get_landline_tariff(country):
     # <li class="btn_arrow_tab left active  ui-state-default ui-corner-top">
     #     <a href="#paymonthlyTariffPlan" id="paymonthly" class="doubleText">Pay Monthly<small>View standard rates and Bolt Ons</small></a>
     # </li>
+    raise Exception("countryName")
 
-    # select Pay Monthly
+    # select Pay Monthly #paymonthly
     elem = driver.find_element_by_name("paymonthly")
-    elem.send_keys(country)
+    #elem.send_keys(country)
     elem.send_keys(Keys.RETURN)
+    raise Exception("paymonthly")
 
-    # find tariff
+    # find tariff in #standardRatesTable
     elem = driver.find_element_by_name("standardRatesTable")
-    elem.send_keys(country)
+    #elem.send_keys(country)
     elem.send_keys(Keys.RETURN)
+    raise Exception("standardRatesTable")
 
 
     #assert "No results found." not in driver.page_source
     driver.close()
     return 
 
-
-
+countries = ["Canada", "Germany", "Iceland", "Pakistan", "Singapore", "South Africa"]
 for country in range(countries):
-    print "landline tariff for" + country + " is: " + get_landline_tariff(country)
-
+    try:
+        print "O2's landline tariff for" + country + " is: " + get_o2_landline_tariff(country)
+    except Exception as ex:
+        print "An error occurred whilst trying to retrieve a tariff for " + country
+        print "Could not find "+ex.args
 
 
 # example from http://selenium-python.readthedocs.org/getting-started.html
